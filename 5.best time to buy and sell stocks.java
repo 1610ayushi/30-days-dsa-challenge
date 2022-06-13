875ab937f550e674a3db5c87e96687b5dcb1115d
Ayushi Tiwari <tayushi1610@gmail.com>
2022-06-14 00:15:22 +0530

    Update 5.best time to buy and sell stocks.java

--- 5.best time to buy and sell stocks.java
+++ 5.best time to buy and sell stocks.java
@@ -36,3 +36,26 @@ class Solution {
         
     }
 }
+
+
+//solution according to coding ninja
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution{
+    public static int maximumProfit(ArrayList<Integer> prices){
+ int min= Integer.MAX_VALUE;
+    int profit=0;
+    for(int i=0;i<prices.size();i++){
+        if(prices.get(i)<min){
+            min=prices.get(i);
+        }
+        else if(prices.get(i)-min>profit){
+          profit=prices.get(i)-min;  
+        }
+        
+    }
+    return profit;
+    }
+}
